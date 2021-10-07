c41d0e23c69d697a0c5969eb24c1f2d639734448
Polina Osipova <76431671+PolinaOsi@users.noreply.github.com>
2021-10-07 14:04:36 +0700

    Update lab.c

--- lab.c
+++ lab.c
@@ -6,7 +6,7 @@
 
 static const int SUCCESS_OF_CREATION = 0;
 
-void *print(void *whois) {
+void *print_and_free(void *whois) {
     for (int i = 0; i < 10; i++) {
         printf("%s%d\n", (char*)whois, i + 1);
     }
@@ -21,13 +21,13 @@ int main (int  argc, char *argv[]) {
     strcpy(childText, "child ");
     char *parentText = (char*)malloc(sizeof(char)*LENGHT_OF_STRING);
     strcpy(parentText, "parent ");
-    int result_of_creation = pthread_create (&id_of_thread, NULL, print, childText);
+    int result_of_creation = pthread_create (&id_of_thread, NULL, print_and_free, childText);
     if (result_of_creation != SUCCESS_OF_CREATION) {
         perror("Thread was not created!");
         free(childText);
         free(parentText);
         exit(EXIT_FAILURE);
     }
-    print(parentText);
+    print_and_free(parentText);
     pthread_exit(EXIT_SUCCESS);
 }
